75e3b870a482df49d753d69ad9b7f5397a9596ca
antoine.brun <abr@ubiqube.com>
2018-07-18 23:45:29 +0900

    tutorial workflow as presented during DevDay

---
+++ WORKFLOWS/Tutorials/Simple_Firewall/Process_Remove_deny_rule/Tasks/Task_remove_rule.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is necessary to include to use all the in-built libraries of /opt/fmc_repository/Reference/Common
+ */
+require_once '/opt/fmc_repository/Process/Reference/Common/common.php';
+
+/**
+ * List all the parameters required by the task
+ */
+function list_args()
+{
+  create_var_def('rules.0.delete', 'String');
+  create_var_def('rules.0.id', 'String');
+  create_var_def('rules.0.src_ip', 'String');
+  create_var_def('rules.0.dst_port', 'String');
+}
+
+foreach ($context['devices'] as $deviceidRow) {
+  
+  $device_id = substr($deviceidRow['id'], 3);
+
+  $index = 0;
+  foreach ($context['rules'] as $rulesRow) {
+
+    $delete = $rulesRow['delete'];
+    $rule_id = $rulesRow['id'];
+    $rule_src_ip = $rulesRow['src_ip'];
+    $rule_dst_port = $rulesRow['dst_port'];
+    logToFile("************************************************");
+    logToFile("$delete : $rule_id - $rule_src_ip:$rule_dst_port");
+  
+    if ($delete === "true") {
+
+      $object_id = $rule_id;
+      $simple_firewall = array('simple_firewall' => $object_id);
+
+      $response = execute_command_and_verify_response($device_id, CMD_DELETE, $simple_firewall, "DELETE simple_firewall");
+      $response = json_decode($response, true);
+      if ($response['wo_status'] !== ENDED) {
+        $response = prepare_json_response($response['wo_status'], $response['wo_comment'], $context, true);
+	echo $response;
+
+      }
+      unset($context['rules'][$index]);
+
+
+    }
+    $index++;
+  } 
+}
+
+
+/**
+ * End of the task do not modify after this point
+ */
+task_exit(ENDED, "Task OK");
+
+?>
