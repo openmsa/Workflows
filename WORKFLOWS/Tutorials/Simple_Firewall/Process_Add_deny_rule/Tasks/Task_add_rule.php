918ced76a7bd3ccf803543eff25f21de10d7d61b
abr-ubiqube <antoine.brun@ubiqube.com>
2018-07-19 09:54:20 +0200

    add comments and fix formatting

--- WORKFLOWS/Tutorials/Simple_Firewall/Process_Add_deny_rule/Tasks/Task_add_rule.php
+++ WORKFLOWS/Tutorials/Simple_Firewall/Process_Add_deny_rule/Tasks/Task_add_rule.php
@@ -6,61 +6,69 @@
 require_once '/opt/fmc_repository/Process/Reference/Common/common.php';
 
 /**
- * List all the parameters required by the task
+ * list all the parameters required by the task
  */
-function list_args()
-{
-	create_var_def('id', 'String');
-	create_var_def('src_ip', 'String');
-	create_var_def('dst_port', 'String');
+function list_args() {
+	create_var_def ( 'id', 'String' );
+	create_var_def ( 'src_ip', 'String' );
+	create_var_def ( 'dst_port', 'String' );
 }
-
-foreach ($context['devices'] as $deviceidRow) {
-
-  $devicelongid = substr($deviceidRow['id'], 3);
-  logToFile("***************************");
-  logToFile("update device $devicelongid");
-
 /**
- * build the Microservice JSON params for the CREATE
+ * iterate through the array of devices in order to apply the policy for each device
  */
-  $micro_service_vars_array = array();
-  $micro_service_vars_array['object_id'] = $context['id'];
-  $micro_service_vars_array['src_ip'] = $context['src_ip'];
-  $micro_service_vars_array['dst_port'] = $context['dst_port'];
-
-  $object_id = $context['id'];
-
-  $simple_firewall = array('simple_firewall' => array($object_id => $micro_service_vars_array));
-
-
-/**
- * call the CREATE for simple_firewall MS for each device
- */
-  $response = execute_command_and_verify_response($devicelongid, CMD_CREATE, $simple_firewall, "CREATE simple_firewall");
-  $response = json_decode($response, true);
-  if ($response['wo_status'] === ENDED) {
-   if (isset($context['rules'])) {
-     $index = count($context['rules']);
-   } else {
-     $index = 0;
-   }
-
-   $context['rules'][$index]['delete'] = false;
-   $context['rules'][$index]['id'] = $context['id'];
-   $context['rules'][$index]['src_ip'] = $context['src_ip'];
-   $context['rules'][$index]['dst_port'] = $context['dst_port'];
-    
-    $response = prepare_json_response($response['wo_status'], $response['wo_comment'], $context, true);
-    echo $response;
-  } else {
-    task_exit(FAILED, "Task FAILED");
-  }
+foreach ( $context ['devices'] as $deviceidRow ) {
+	/**
+	 * extract the device database identifier from the device ID
+	 */
+	$devicelongid = substr ( $deviceidRow ['id'], 3 );
+	logToFile ( "***************************" );
+	logToFile ( "update device $devicelongid" );
+	
+	/**
+	 * build the Microservice JSON params for the CREATE operation of the microservice
+	 */
+	$micro_service_vars_array = array ();
+	$micro_service_vars_array ['object_id'] = $context ['id'];
+	$micro_service_vars_array ['src_ip'] = $context ['src_ip'];
+	$micro_service_vars_array ['dst_port'] = $context ['dst_port'];
+	
+	$object_id = $context ['id'];
+	
+	$simple_firewall = array (
+			'simple_firewall' => array (
+					$object_id => $micro_service_vars_array 
+			) 
+	);
+	
+	/**
+	 * call the CREATE for simple_firewall MS for each device
+	 */
+	$response = execute_command_and_verify_response ( $devicelongid, CMD_CREATE, $simple_firewall, "CREATE simple_firewall" );
+	$response = json_decode ( $response, true );
+	if ($response ['wo_status'] === ENDED) {
+		if (isset ( $context ['rules'] )) {
+			$index = count ( $context ['rules'] );
+		} else {
+			$index = 0;
+		}
+		/**
+		 * add the firewall policy rule to the array of rules applied on the devices
+		 */
+		$context ['rules'] [$index] ['delete'] = false;
+		$context ['rules'] [$index] ['id'] = $context ['id'];
+		$context ['rules'] [$index] ['src_ip'] = $context ['src_ip'];
+		$context ['rules'] [$index] ['dst_port'] = $context ['dst_port'];
+		
+		$response = prepare_json_response ( $response ['wo_status'], $response ['wo_comment'], $context, true );
+		echo $response;
+	} else {
+		task_exit ( FAILED, "Task FAILED" );
+	}
 }
 
 /**
  * End of the task do not modify after this point
  */
-task_exit(ENDED, "Task OK");
+task_exit ( ENDED, "Task OK" );
 
 ?>
