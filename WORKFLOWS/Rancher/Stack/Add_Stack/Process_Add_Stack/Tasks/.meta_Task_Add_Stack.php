aa845735bc9c045b5144dcc4a92da21344c3767b
Sergio LEBON <sle@ubiqube.com>
2018-09-17 17:08:28 +0200

    Add vRO Rancher workflows.

---
+++ WORKFLOWS/Rancher/Stack/Add_Stack/Process_Add_Stack/Tasks/.meta_Task_Add_Stack.php
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<metadata>
+    <map>
+        <entry>
+            <key>FILE_TYPE</key>
+            <value>text</value>
+        </entry>
+        <entry>
+            <key>DATE_MODIFICATION</key>
+            <value>1536864002421</value>
+        </entry>
+        <entry>
+            <key>COMMENT</key>
+            <value></value>
+        </entry>
+        <entry>
+            <key>DATE_CREATION</key>
+            <value>1536863854246</value>
+        </entry>
+        <entry>
+            <key>TAG</key>
+            <value></value>
+        </entry>
+        <entry>
+            <key>TYPE</key>
+            <value>UPLOAD</value>
+        </entry>
+    </map>
+</metadata>
