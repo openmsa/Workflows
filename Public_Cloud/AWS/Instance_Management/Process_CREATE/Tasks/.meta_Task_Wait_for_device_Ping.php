193f9e9954e740cc827ecc44513830fa6b41aaeb
abr-ubiqube <antoine.brun@openmsa.co>
2019-12-18 15:42:07 +0000

    add meta files

--- Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/.meta_Task_Wait_for_device_Ping.php
+++ Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/.meta_Task_Wait_for_device_Ping.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1559831212541</value>
+            <value>1575991172375</value>
         </entry>
         <entry>
             <key>COMMENT</key>
