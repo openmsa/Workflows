41508b1b2302a5649bf4c042dd7b8ec1b026add7
Dmitrii Overchenko <49155524+n1654@users.noreply.github.com>
2020-05-06 16:55:29 +0300

    response fixed, context rules accumulated

--- Tutorials/python/Simple_Firewall/Add_filter_Rule_add_rule.py
+++ Tutorials/python/Simple_Firewall/Add_filter_Rule_add_rule.py
@@ -10,42 +10,54 @@ dev_var.add('src_ip', var_type='String')
 dev_var.add('dst_port', var_type='Integer')
 context = Variables.task_call(dev_var)
 
-# read the ID of the selected managed entity  
+# read the ID of the selected managed entity
 device_id = context['device']
-
 # extract the database ID
 devicelongid = device_id[-3:]
 
 # logToFile("update device $devicelongid");
+
 # build the Microservice JSON params for the CREATE
-micro_service_vars_array = {'object_id': context['id'],
-                            'src_ip': context['src_ip'],
-                            'dst_port': context['dst_port']
+micro_service_vars_array = {"object_id": context['id'],
+                            "src_ip": context['src_ip'],
+                            "dst_port": context['dst_port']
                             }
+
 object_id = context['id']
-simple_firewall = {'simple_firewall': {object_id: micro_service_vars_array}}
+
+simple_firewall = {"simple_firewall": {object_id: micro_service_vars_array}}
 
 # call the CREATE for simple_firewall MS for each device
 order = Order(devicelongid)
-
-# command_execute(self, command, params, timeout=60) 
+# command_execute(self, command, params, timeout=60)
 order.command_execute('CREATE', json.dumps(simple_firewall))
-response = json.loads(order.content)
-if 'wo_status' not in response.keys():
-  ret = MSA_API.process_content('FAILED', f'Policy update failed - {response}', context, True)
-elif response['wo_status'] is 'ENDED':
-  if 'rules' not in context.keys() or conext['rules'] is '':
-    index = 0
-  else:
-    index = len(context['rules'])
-  context['rules'] = {index: {
-                              'delete': False,
-                              'id': context['id'],
-                              'src_ip': context['src_ip'],
-                              'dst_port': context['dst_port']
-                             }
-                       }
-  ret = MSA_API.process_content(response['wo_status'], response['wo_comment'], context, True)
+
+# convert dict object into json
+content = json.loads(order.content)
+
+if order.response.status_code == 200:
+
+    if 'rules' in context.keys():
+        num = len(context['rules'])
+    else:
+        context['rules'] = {}
+        num = 0
+
+    context['rules'][num] = {}
+    context['rules'][num]['delete'] = False
+    context['rules'][num]['id'] = context['id']
+    context['rules'][num]['src_ip'] = context['src_ip']
+    context['rules'][num]['dst_port'] = context['dst_port']
+
+    ret = MSA_API.process_content('ENDED',
+                                  f'STATUS: {content["status"]}, \
+                                    MESSAGE: {content["message"]}',
+                                  context, True)
 else:
-  ret = MSA_API.process_content('FAILED', f'Policy update failed - {response}', context, True)
+    ret = MSA_API.process_content('FAILED',
+                                  f'Policy update failed \
+                                  - {order.content}',
+                                  context, True)
+
+
 print(ret)
