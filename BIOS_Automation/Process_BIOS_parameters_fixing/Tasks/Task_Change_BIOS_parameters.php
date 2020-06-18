6984656328832c60e4c8b8b480fbc17117e2f8c1
abr-ubiqube <antoine.brun@ubiqube.com>
2020-06-18 13:17:59 +0000

    add BIOS automation demo WF

---
+++ BIOS_Automation/Process_BIOS_parameters_fixing/Tasks/Task_Change_BIOS_parameters.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once '/opt/fmc_repository/Process/Reference/Common/common.php';
+require_once '/opt/fmc_repository/Process/Reference/Common/Library/msa_common.php';
+
+//Retrive variables from $context and define the new ones
+$delay = 5;
+$bios_parameters = $context['bios_parameters_array'];
+$device_id = $context['device_id'];
+$microservices_array = $context['microservices_array'];
+$ms_bios_params = $microservices_array['BIOS parameters manipulation'];
+
+//Change BIOS parameters for each parameter what has was_it_changed is True
+foreach ($bios_parameters as $parameter_name => $parameter_values) {
+	if ($parameter_values['was_it_changed'] === "true") {
+		$micro_service_vars_array = array ();
+		$micro_service_vars_array ['object_id'] = $parameter_name;
+		$micro_service_vars_array ['value'] = $parameter_values['Required Value'];
+		$ms_array = array($ms_bios_params => array ($parameter_name => $micro_service_vars_array));
+		$response = json_decode(execute_command_and_verify_response ( $device_id, CMD_UPDATE, $ms_array, "UPDATE BIOS parameter ".$parameter_name." to ".$parameter_values['Required Value']), True);
+
+    	if ($response['wo_status'] !== ENDED) {
+    	    $response = json_encode($response);
+    	    echo $response;
+    	    exit;
+    	}
+	}
+	sleep($delay);
+}
+
+task_success('BIOS parameters have been changed succesfully. Server will be rebooted');
+
+?>
