65a3e45b33c99ba29c51de4c75e414cb4c879e19
Aleksandr Shilkin <asi@ubiqube.com>
2020-07-27 13:52:38 +0300

    Workflow creates ME with specific port

---
+++ BIOS_Automation/Create_configuration_profile/Create_profile_Attache_microservices_to_profile.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file is necessary to include to use all the in-built libraries of /opt/fmc_repository/Reference/Common
+ */
+require_once '/opt/fmc_repository/Process/Reference/Common/common.php';
+
+
+
+
+
+task_success('Task OK');
+task_error('Task FAILED');
+?>
