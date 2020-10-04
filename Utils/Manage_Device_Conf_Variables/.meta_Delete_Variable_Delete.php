4448f6071b2f0893e70059f42f0e55a6dc0ff50c
Antoine Brun <antoine.brun@openmsa.co>
2020-10-04 15:32:50 +0200

    Update WF for managing ME config vars (#105)
    * list vars in an array
    * rename and cleanup
    * add description
    * rename task delete
    * update var array after any update/delete/create
    * move function to update var array in common PHP
    * add require_once for Common.php
    * add meta files
    * update path name
    
    Co-authored-by: antoine-brun <antoine.brun@gmail.com>

--- Utils/Manage_Device_Conf_Variables/.meta_Delete_Variable_Delete.php
+++ Utils/Manage_Device_Conf_Variables/.meta_Delete_Variable_Delete.php
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1594912013084</value>
+            <value>1601815262124</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1594912013080</value>
+            <value>1601815262119</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Utils/Manage_Device_Conf_Variables/Update_Variable_Update.php
+++ Utils/Manage_Device_Conf_Variables/Update_Variable_Update.php
@@ -3,26 +3,25 @@
 
 require_once '/opt/fmc_repository/Process/Reference/Common/common.php';
 require_once '/opt/fmc_repository/Process/Reference/Common/Library/msa_common.php';
+require_once 'Common.php';
 
 
-/*
-The process recieves server IP and MAC address as parameters
-*/
 function list_args()
 {
+  create_var_def('variable_name', 'String');
   create_var_def('variable_value', 'String');
 }
 
-
 $device_id = $context['device_id'];
 $variable_name = $context['variable_name'];
-$variable_type = $context['variable_type'];
 $variable_value = $context['variable_value'];
 
 _configuration_variable_delete ($device_id, $variable_name);
 
-_configuration_variable_create ($device_id, $variable_name, $variable_value, $type =$variable_type, $comment = "");
+_configuration_variable_create ($device_id, $variable_name, $variable_value, $type = "String", $comment = "");
 
-task_success('The variable '.$variable_name.'(type is '.$variable_type.') with value '.$variable_value.' has been updated and added to device '.$device_id.' successfully');
+update_var_array($device_id);
+
+task_success('The variable '.$variable_name.' with value '.$variable_value.' has been successfully updated and added to device '.$device_id);
 
 ?>
