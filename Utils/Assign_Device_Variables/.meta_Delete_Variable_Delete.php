9752da934082fa8d666de3c722c886473232a365
root <devops@openmsa.co>
2020-10-04 12:57:28 +0000

    add meta files

--- Utils/Assign_Device_Variables/.meta_Delete_Variable_Delete.php
+++ Utils/Assign_Device_Variables/.meta_Delete_Variable_Delete.php
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1601814017694</value>
+            <value>1601815262124</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1601814017688</value>
+            <value>1601815262119</value>
         </entry>
         <entry>
             <key>TAG</key>
