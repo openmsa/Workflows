413a79fb634fa8a8af17a031beb83ca022f3fd26
root <devops@openmsa.co>
2020-10-05 12:34:41 +0000

    save

--- Ticketing/Jira_Integration/.meta_Create_Ticket_Create_new_Ticket.php
+++ Ticketing/Jira_Integration/.meta_Create_Ticket_Create_new_Ticket.php
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1601468343038</value>
+            <value>1601900503989</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1601468343027</value>
+            <value>1601900503978</value>
         </entry>
         <entry>
             <key>TAG</key>
