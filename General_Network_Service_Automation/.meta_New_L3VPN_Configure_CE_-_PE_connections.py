b7147e4b5a55d88217aa70bca8d80be1f4a9a2d4
root <devops@openmsa.co>
2020-10-06 12:54:12 +0000

    Adopted to 2.2.0GA

--- General_Network_Service_Automation/.meta_New_L3VPN_Configure_CE_-_PE_connections.py
+++ General_Network_Service_Automation/.meta_New_L3VPN_Configure_CE_-_PE_connections.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1601491661257</value>
+            <value>1601980973728</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1601491661250</value>
+            <value>1601980973719</value>
         </entry>
         <entry>
             <key>TAG</key>
