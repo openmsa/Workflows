72b40634b295b395b0469cf03b927132818b7cb0
root <devops@openmsa.co>
2020-10-27 18:54:30 +0000

    Minor Changes

--- General_Network_Service_Automation/.meta_New_CE_Create_CE_device.py
+++ General_Network_Service_Automation/.meta_New_CE_Create_CE_device.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1602160629435</value>
+            <value>1603818810230</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1602160629425</value>
+            <value>1603818810224</value>
         </entry>
         <entry>
             <key>TAG</key>
