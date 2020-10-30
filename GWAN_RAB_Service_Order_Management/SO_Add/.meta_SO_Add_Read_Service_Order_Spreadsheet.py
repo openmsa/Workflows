5d66795a5b25df6f1d1bb224298cd4a421c0d38d
Sergio LEBON <sle@ubiqube.com>
2020-10-30 17:32:17 +0100

    MSA-9063: update the code.

--- GWAN_RAB_Service_Order_Management/SO_Add/.meta_SO_Add_Read_Service_Order_Spreadsheet.py
+++ GWAN_RAB_Service_Order_Management/SO_Add/.meta_SO_Add_Read_Service_Order_Spreadsheet.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1603788091268</value>
+            <value>1604064465096</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1603788091265</value>
+            <value>1604064465091</value>
         </entry>
         <entry>
             <key>TAG</key>
