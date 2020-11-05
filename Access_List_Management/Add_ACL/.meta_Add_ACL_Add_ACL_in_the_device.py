d637313ef192323c0015f2f2427e2499de6d7ba6
Sergio Lebon <sle@ubiqube.com>
2020-11-05 17:18:58 +0100

    MSA-9064: create 'add/delete ACL' baseline WF process tasks.

--- Access_List_Management/Add_ACL/.meta_Add_ACL_Add_ACL_in_the_device.py
+++ Access_List_Management/Add_ACL/.meta_Add_ACL_Add_ACL_in_the_device.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1604316146824</value>
+            <value>1604590473563</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1604316146820</value>
+            <value>1604590473559</value>
         </entry>
         <entry>
             <key>TAG</key>
