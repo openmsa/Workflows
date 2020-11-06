6b6f0b65522f10cee00e506b1897a2e65eea8af0
root <abr@ubiqube.com>
2020-11-06 10:44:02 +0000

    add meta files

--- Tutorials/python/Simple_Firewall/.meta_Add_filter_Rule_add_rule.py
+++ Tutorials/python/Simple_Firewall/.meta_Add_filter_Rule_add_rule.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1588749810433</value>
+            <value>1604657524699</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1588749810426</value>
+            <value>1604657524693</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Tutorials/python/Simple_Firewall/.meta_Create_Firewall_Service_Select_Devices.py
+++ Tutorials/python/Simple_Firewall/.meta_Create_Firewall_Service_Select_Devices.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1604592700925</value>
+            <value>1604656595492</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1604592700919</value>
+            <value>1604656595482</value>
         </entry>
         <entry>
             <key>TAG</key>
