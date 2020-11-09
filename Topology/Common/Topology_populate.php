56f0ff210aca89253a3a3c10653e91f2ce5ffd9b
antoine-brun <antoine.brun@gmail.com>
2020-11-09 11:39:54 +0100

    stop setting images and color in PHP

--- Topology/Common/Topology_populate.php
+++ Topology/Common/Topology_populate.php
@@ -56,7 +56,7 @@ function createTopology($nodeId, $name, $device_nature, $subtype, $image="") {
 /*
  * create a new topology element for the network
  */
-function createTopologyNetwork($nodeId, $name, $subtype, $image) {
+function createTopologyNetwork($nodeId, $name, $subtype, $image="") {
 	global $context;
 
 	$place = _topology_exist_object_this_instance($nodeId);
@@ -79,7 +79,7 @@ function createTopologyNetwork($nodeId, $name, $subtype, $image) {
 	} else {
 		unset($context['Nodes'][$place]["link"]);
 		$context['Nodes'][$place]["name"] = $name;
-		$context['Nodes'][$place]["image"] = $image;
+		//$context['Nodes'][$place]["image"] = $image;
 	}
 
 	$context['Nodes_MAJ'][] = array(
