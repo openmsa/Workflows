cb8208bfabf1023d6da12a8176aa1ee0cae6cf8d
Sergio Lebon <sle@ubiqube.com>
2020-11-10 09:32:40 +0100

    MSA-9149: fix display multiple spreadsheet files in tab list bug.

--- GWAN_RAB_Service_Order_Management/SO_Instantiate/SO_Instantiate_Spreadsheet_List_Acquisition.py
+++ GWAN_RAB_Service_Order_Management/SO_Instantiate/SO_Instantiate_Spreadsheet_List_Acquisition.py
@@ -51,10 +51,10 @@ spreadsheets_directory = context['spreadsheets_directory']
 spreadsheets_extension = context['spreadsheets_extension']
 
 if not spreadsheets_directory:
-	spreadsheets_directory = '/opt/fmc_repository/Datafiles/GWAN_RAB'
+    spreadsheets_directory = '/opt/fmc_repository/Datafiles/GWAN_RAB'
   
 if not spreadsheets_extension:
-	spreadsheets_extension = '.xlsx'
+    spreadsheets_extension = '.xlsx'
 
 #list the spreadsheet files from specified directory in the MSA repositary data.  
 pattern = spreadsheets_directory + '/*' + spreadsheets_extension
@@ -62,25 +62,26 @@ spreadsheet_list = glob.glob(pattern)
 
 # check if at least one spreadsheet file is available. If not exit and return failed.
 if not spreadsheet_list:
-	ret = MSA_API.process_content('FAILED', 'No spreadsheed file found from \'' + spreadsheets_directory + '\' directory.', context, True)
-	print(ret)	
+    ret = MSA_API.process_content('FAILED', 'No spreadsheed file found from \'' + spreadsheets_directory + '\' directory.', context, True)
+    print(ret)	
 
 #spreadsheet_list_restructured = [dict(spreadsheet=st, is_selected='false') for st in spreadsheet_list]
+spreadsheet_list_restructured = []
 
 for st in spreadsheet_list:
-	df = pandas.read_excel('file:' + st, sheet_name=SHEET_NAME_CONTAINING_DEVICE_REF)
-	#defined the position of the device reference from the sheet name 'Main'
-	device_ext_ref = get_device_reference_from_sheet(df, SHEET_DEVICE_REF_ROW_INDEX, FILTER_COLUMN_NAME)
-	#device_external_ref is mandatory, if it is empty return FAILED.
-	if not device_ext_ref:
-		ret = MSA_API.process_content('FAILED', 'Device external reference (hostname) is empty from sheet called ' + SHEET_NAME_CONTAINING_DEVICE_REF + '.', context, True)
-		print(ret)
-	#create spreadsheet dictionaries list.
-	spreadsheet_basename = osp.basename(st)
-	spreadsheet_list_restructured = [dict(spreadsheet=spreadsheet_basename, is_selected='false', device_external_ref=device_ext_ref)]
-	#store in the context 'device_external_ref' and 'spreadsheet_list'
-	#context['device_external_ref'] = device_ext_ref
-	context['spreadsheet_list'] = spreadsheet_list_restructured
+    df = pandas.read_excel('file:' + st, sheet_name=SHEET_NAME_CONTAINING_DEVICE_REF)
+    #defined the position of the device reference from the sheet name 'Main'
+    device_ext_ref = get_device_reference_from_sheet(df, SHEET_DEVICE_REF_ROW_INDEX, FILTER_COLUMN_NAME)
+    #device_external_ref is mandatory, if it is empty return FAILED.
+    if not device_ext_ref:
+        ret = MSA_API.process_content('FAILED', 'Device external reference (hostname) is empty from sheet called ' + SHEET_NAME_CONTAINING_DEVICE_REF + '.', context, True)
+        print(ret)
+    #create spreadsheet dictionaries list.
+    spreadsheet_basename = osp.basename(st)
+    spreadsheet_dict = dict(spreadsheet=spreadsheet_basename, is_selected='false', device_external_ref=device_ext_ref)
+    spreadsheet_list_restructured.append(spreadsheet_dict)
+#store in the context 'device_external_ref' and 'spreadsheet_list'
+context['spreadsheet_list'] = spreadsheet_list_restructured
 
 
 ret = MSA_API.process_content('ENDED', 'Spreadsheet list acquisition is done successfully.', context, True)
