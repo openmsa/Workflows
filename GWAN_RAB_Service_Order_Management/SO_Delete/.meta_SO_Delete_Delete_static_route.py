8bd74a6e0a5a0f955f1406c16aac74d28638a181
Sergio Lebon <sle@ubiqube.com>
2020-11-12 16:11:00 +0100

    MSA-9149: update execute_service_by_reference response handling.

--- GWAN_RAB_Service_Order_Management/SO_Delete/.meta_SO_Delete_Delete_static_route.py
+++ GWAN_RAB_Service_Order_Management/SO_Delete/.meta_SO_Delete_Delete_static_route.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1604659983330</value>
+            <value>1605191422682</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1604659983326</value>
+            <value>1605191422678</value>
         </entry>
         <entry>
             <key>TAG</key>
