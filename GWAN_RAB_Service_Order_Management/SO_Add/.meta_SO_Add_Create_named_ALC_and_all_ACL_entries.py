68b9cc37be87c1ec3a1a6cf18cb17c78813b77ee
Sergio Lebon <sle@ubiqube.com>
2020-11-12 15:34:56 +0100

    MSA-9149: update SO ADD/DEL - acl name and entries processes tasks.

--- GWAN_RAB_Service_Order_Management/SO_Add/.meta_SO_Add_Create_named_ALC_and_all_ACL_entries.py
+++ GWAN_RAB_Service_Order_Management/SO_Add/.meta_SO_Add_Create_named_ALC_and_all_ACL_entries.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1603788889568</value>
+            <value>1605025661577</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1603788889565</value>
+            <value>1605025661562</value>
         </entry>
         <entry>
             <key>TAG</key>
--- GWAN_RAB_Service_Order_Management/SO_Delete/.meta_SO_Delete_Delete_ACL.py
+++ GWAN_RAB_Service_Order_Management/SO_Delete/.meta_SO_Delete_Delete_ACL.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1603789744283</value>
+            <value>1605178142646</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1603789744279</value>
+            <value>1605178142642</value>
         </entry>
         <entry>
             <key>TAG</key>
