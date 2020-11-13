3ff6614efa46ce2ffc1ef731212dc36300bc8503
Sergio Lebon <sle@ubiqube.com>
2020-11-13 11:15:26 +0100

    MSA-9149: rename MS parameter 'policy_name' to 'policy_map'.

--- Service_Policy_Management/Delete_Service_Policy/.meta_Delete_Service_Policy_Delete_Service_Policy_in_the_device.py
+++ Service_Policy_Management/Delete_Service_Policy/.meta_Delete_Service_Policy_Delete_Service_Policy_in_the_device.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1604585784732</value>
+            <value>1605261190458</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1604585784726</value>
+            <value>1605261190455</value>
         </entry>
         <entry>
             <key>TAG</key>
