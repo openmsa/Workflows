757445e49685410df58d1a31df393d6e27aea184
Sergio Lebon <sle@ubiqube.com>
2020-11-18 16:18:17 +0100

    MSA-9174: update class-map management WF tasks 'Ensure Class Map does or not exist in the device'.

--- Class_Map_Management/Common/.meta_Common_Ensure_Class_Map_do_not_exist_in_the_device.py
+++ Class_Map_Management/Common/.meta_Common_Ensure_Class_Map_do_not_exist_in_the_device.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1604321498299</value>
+            <value>1605709098558</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1604321498295</value>
+            <value>1605709098554</value>
         </entry>
         <entry>
             <key>TAG</key>
