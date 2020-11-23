f2a5d9104433961e7fdadd19102e615c8b21c4b4
Sergio Lebon <sle@ubiqube.com>
2020-11-23 16:07:40 +0100

    update Policy_Map_Management WF Ensure_Policy_Map* tasks skeleton.

--- Policy_Map_Management/Common/.meta_Common_Ensure_Policy_Map_do_not_exist_in_the_device.py
+++ Policy_Map_Management/Common/.meta_Common_Ensure_Policy_Map_do_not_exist_in_the_device.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1604321946344</value>
+            <value>1606143423521</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1604321946340</value>
+            <value>1606143423516</value>
         </entry>
         <entry>
             <key>TAG</key>
