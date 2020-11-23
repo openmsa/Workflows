041ac6ac8532702ff7886fe2696facfec9569d7e
Sergio Lebon <sle@ubiqube.com>
2020-11-23 14:12:22 +0100

    MSA-9252: implement tasks Common_Ensure_Static_Routing_do_not_exist_in_the_device + Common_Ensure_Static_Routing_exist_in_the_device.

--- Static_Routing_Management/Common/.meta_Common_Ensure_Static_Routing_do_not_exist_in_the_device.py
+++ Static_Routing_Management/Common/.meta_Common_Ensure_Static_Routing_do_not_exist_in_the_device.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1604306798589</value>
+            <value>1605886209270</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1604306798585</value>
+            <value>1605886209265</value>
         </entry>
         <entry>
             <key>TAG</key>
