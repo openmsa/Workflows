731e9db5753766ec5e9951f0495b06c0c17e471e
Sergio Lebon <sle@ubiqube.com>
2020-11-24 11:55:09 +0100

    MSA-9060: update Class_Map_Management WF to get response from order class object.

--- Class_Map_Management/Delete_Class_Map/.meta_Delete_Class_Map_Delete_Class_Map_in_the_device.py
+++ Class_Map_Management/Delete_Class_Map/.meta_Delete_Class_Map_Delete_Class_Map_in_the_device.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1604321518099</value>
+            <value>1606213664158</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1604321518095</value>
+            <value>1606213664154</value>
         </entry>
         <entry>
             <key>TAG</key>
