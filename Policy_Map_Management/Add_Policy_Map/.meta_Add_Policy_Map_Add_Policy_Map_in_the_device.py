1bf443b7c0c8a481a4811ea30f53ffb7075b37de
Sergio Lebon <sle@ubiqube.com>
2020-11-24 11:52:56 +0100

    MSA-9068: update Policy_Map_Management WF to get response from order class object.

--- Policy_Map_Management/Add_Policy_Map/.meta_Add_Policy_Map_Add_Policy_Map_in_the_device.py
+++ Policy_Map_Management/Add_Policy_Map/.meta_Add_Policy_Map_Add_Policy_Map_in_the_device.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1605539790372</value>
+            <value>1606213586274</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1605539790368</value>
+            <value>1606213586270</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Policy_Map_Management/Delete_Policy_Map/.meta_Delete_Policy_Map_Delete_Policy_Map_in_the_device.py
+++ Policy_Map_Management/Delete_Policy_Map/.meta_Delete_Policy_Map_Delete_Policy_Map_in_the_device.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1605541414602</value>
+            <value>1606213574044</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1605541414597</value>
+            <value>1606213574040</value>
         </entry>
         <entry>
             <key>TAG</key>
