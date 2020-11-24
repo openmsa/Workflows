e2cf4b09177df2cce44941bcb1eab66cb399c814
Sergio Lebon <sle@ubiqube.com>
2020-11-24 11:24:54 +0100

    MSA-9252: update Static Routing Management WF to get response from order class object.

--- Static_Routing_Management/Delete_Static_Route/.meta_Delete_Static_Route_Delete_Static_Routing_in_the_device.py
+++ Static_Routing_Management/Delete_Static_Route/.meta_Delete_Static_Route_Delete_Static_Routing_in_the_device.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1604571605827</value>
+            <value>1606211746096</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1604571605823</value>
+            <value>1606211746092</value>
         </entry>
         <entry>
             <key>TAG</key>
