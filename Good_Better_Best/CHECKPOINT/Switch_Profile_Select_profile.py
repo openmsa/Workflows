06b8ede0e97dfc7cf8847dd155e47ca7d3677dfb
root <antoine.brun@ubiqube.com>
2020-11-26 10:54:28 +0000

    fix WF definition

--- Good_Better_Best/CHECKPOINT/Switch_Profile_Select_profile.py
+++ Good_Better_Best/CHECKPOINT/Switch_Profile_Select_profile.py
@@ -17,72 +17,8 @@ profile = context['profile']
 # extract the database ID
 devicelongid = device_id[-3:]
 
-# build the Microservice JSON params for the CREATE
-#{"Gateway":{"undefined":{"name":"hgu001","ipv4_address":"1.1.1.1","firewall":true,"vpn":true}}}
-micro_service_vars_array = {"object_id":object_id,
-                            "firewall":"true",
-                            "vpn":"true",
-                            "application_control":"true",
-                            "ips":"true",
-                            "content_awareness":"true",
-                            "url_filtering":"false",
-                            "anti_virus":"false",
-                            "anti_bot":"false",
-                            "anti_spam":"false",
-                            "threat_emulation":"false",
-                            "threat_extraction":"false"
-                            }
-
-#object_id = context['id']
-if profile == "better":
-  micro_service_vars_array = {"object_id":object_id,
-                            "firewall":"true",
-                            "vpn":"true",
-                            "application_control":"true",
-                            "ips":"true",
-                            "content_awareness":"true",
-                            "url_filtering":"true",
-                            "anti_virus":"true",
-                            "anti_bot":"true",
-                            "anti_spam":"true",
-                            "threat_emulation":"false",
-                            "threat_extraction":"false"
-                            }
-elif profile == "best":
-  micro_service_vars_array = {"object_id":object_id,
-                            "firewall":"true",
-                            "vpn":"true",
-                            "application_control":"true",
-                            "ips":"true",
-                            "content_awareness":"true",
-                            "url_filtering":"true",
-                            "anti_virus":"true",
-                            "anti_bot":"true",
-                            "anti_spam":"true",
-                            "threat_emulation":"true",
-                            "threat_extraction":"true"
-                            }
-
-gateway = {"Gateway": {object_id: micro_service_vars_array}}
-
-# call the CREATE for simple_firewall MS for each device
-order = Order(devicelongid)
-order.command_execute('UPDATE', gateway)
-
-# convert dict object into json
-content = json.loads(order.content)
-
-# check if the response is OK
-if order.response.ok:
-    ret = MSA_API.process_content('ENDED',
-                                  f'STATUS: {content["status"]}, \
-                                    MESSAGE: Profile {profile}',
-                                  context, True)
-else:
-    ret = MSA_API.process_content('FAILED',
-                                  f'Gateway update failed \
-                                  - {order.content}',
-                                  context, True)
-
+ret = MSA_API.process_content('ENDED',
+                              f'STATUS: OK, MESSAGE: Profile {profile}',
+                              context, True)
 
 print(ret)
