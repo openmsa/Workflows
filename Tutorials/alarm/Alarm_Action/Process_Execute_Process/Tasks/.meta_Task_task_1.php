92a67307ee7039e8c1f09e03e13746cde1294bb3
root <antoine.brun@ubiqube.com>
2021-01-13 16:12:13 +0000

    initial commit

---
+++ Tutorials/alarm/Alarm_Action/Process_Execute_Process/Tasks/.meta_Task_task_1.php
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<metadata>
+    <map>
+        <entry>
+            <key>DISPLAYNAME</key>
+            <value>Task_task_1.php</value>
+        </entry>
+        <entry>
+            <key>DATE_MODIFICATION</key>
+            <value>1610551551011</value>
+        </entry>
+        <entry>
+            <key>REPOSITORY</key>
+            <value>Process</value>
+        </entry>
+        <entry>
+            <key>DATE_CREATION</key>
+            <value>1610551551006</value>
+        </entry>
+        <entry>
+            <key>TAG</key>
+        </entry>
+        <entry>
+            <key>FILE_TYPE</key>
+            <value>text</value>
+        </entry>
+        <entry>
+            <key>TYPE</key>
+            <value>UPLOAD</value>
+        </entry>
+        <entry>
+            <key>COMMENT</key>
+        </entry>
+    </map>
+</metadata>
