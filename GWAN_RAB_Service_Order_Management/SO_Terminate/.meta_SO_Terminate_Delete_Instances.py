6c9d82d5e52f82ffd37323483039db371463c478
root <devops@openmsa.co>
2021-01-21 09:17:24 +0000

    Ignore to remove baseline service instance if does not exist.

--- GWAN_RAB_Service_Order_Management/SO_Terminate/.meta_SO_Terminate_Delete_Instances.py
+++ GWAN_RAB_Service_Order_Management/SO_Terminate/.meta_SO_Terminate_Delete_Instances.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1609855429560</value>
+            <value>1611070809533</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1609855429552</value>
+            <value>1611070809529</value>
         </entry>
         <entry>
             <key>TAG</key>
