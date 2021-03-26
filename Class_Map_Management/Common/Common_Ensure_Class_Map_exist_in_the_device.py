102aa988a70d87ba62e8f003b206daa8aea64e1f
Sergio Lebon <sle@ubiqube.com>
2021-03-26 15:48:14 +0100

    MSA-9787_MSA-9788: update class-map create and delete failure conditions.

--- Class_Map_Management/Common/Common_Ensure_Class_Map_exist_in_the_device.py
+++ Class_Map_Management/Common/Common_Ensure_Class_Map_exist_in_the_device.py
@@ -29,12 +29,15 @@ is_class_map_name = False
 ret_acl_name = ''
 if response:
     if object_id in response.get(object_name):
-        is_class_map_name = True  	
-        ret_acl_name = response.get(object_name).get(object_id).get('access').get('0').get('acl')
-
+        is_class_map_name = True
+        class_map_obj = response.get(object_name).get(object_id)
+        if 'access' in class_map_obj:
+            if 'acl' in class_map_obj.get('access').get('0'):
+                ret_acl_name = class_map_obj.get('access').get('0').get('acl')
+                context.update(ret_acl_name=ret_acl_name)
 #if response equals empty dictionary it means class map object is not exist in the device yet.
-if not ret_acl_name and is_class_map_name != True:
-    ret = MSA_API.process_content(constants.FAILED, 'Class Map with id="' + object_id + '" does not exist in the device.', context, True)
+if not ret_acl_name or is_class_map_name != True:
+    ret = MSA_API.process_content(constants.FAILED, 'Class Map with id="' + object_id + '" does not exist or is not associated to ACL ' + context.get('acl') + 'in the device.', context, True)
     print(ret)
 ret = MSA_API.process_content(constants.ENDED, 'Class Map with id="' + object_id + '" exists in the device.', context, True)
 print(ret)
