5b431183ddcd3efc0a03f80bd3cc71843ab2b5a3
root <devops@openmsa.co>
2021-03-29 09:37:52 +0000

    meta files

--- Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/.meta_Task_Create_VM.php
+++ Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/.meta_Task_Create_VM.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1616595900117</value>
+            <value>1617009086447</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1616595900101</value>
+            <value>1617009086442</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/.meta_Task_msa_device_create.php
+++ Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/.meta_Task_msa_device_create.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1616605235920</value>
+            <value>1617009565168</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1616596468520</value>
+            <value>1617009565162</value>
         </entry>
         <entry>
             <key>TAG</key>
