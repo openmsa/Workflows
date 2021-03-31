0cd9f386130f2e58901146bf62cb3dfbb8808856
root <devops@openmsa.co>
2021-03-31 11:25:08 +0000

    meta files

--- Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/.meta_Task_msa_device_create.php
+++ Public_Cloud/AWS/Instance_Management/Process_CREATE/Tasks/.meta_Task_msa_device_create.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1617109506521</value>
+            <value>1617189003591</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1617109506516</value>
+            <value>1617189003584</value>
         </entry>
         <entry>
             <key>TAG</key>
