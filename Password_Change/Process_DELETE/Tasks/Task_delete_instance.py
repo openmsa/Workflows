5d0c305fd4d601eaa590c52a7b467036bae147ac
Dmitrii Overchenko <49155524+n1654@users.noreply.github.com>
2021-05-18 18:56:08 +0300

    typo fixed

--- Password_Change/Process_DELETE/Tasks/Task_delete_instance.py
+++ Password_Change/Process_DELETE/Tasks/Task_delete_instance.py
@@ -7,13 +7,3 @@ context = Variables.task_call()
 ret = MSA_API.process_content('ENDED', 'Instance deleted', context, True)
 print(ret)
 
-[root@462c84081ed5 Password_Change]# cat ./Process_DELETE/Tasks/Task_delete_instance.py
-from msa_sdk.variables import Variables
-from msa_sdk.msa_api import MSA_API
-
-dev_var = Variables()
-context = Variables.task_call()
-
-ret = MSA_API.process_content('ENDED', 'Instance deleted', context, True)
-print(ret)
-
