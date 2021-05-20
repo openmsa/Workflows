ec5dba6d683325dc24969c7ac190df1f5092d299
ydu <ydu@ubiqube.com>
2021-05-20 14:22:42 +0200

    fix Create and Update topology tasks

--- Topology/Process_Create_View/Tasks/Task_Create.php
+++ Topology/Process_Create_View/Tasks/Task_Create.php
@@ -14,13 +14,10 @@ function list_args() {
 check_mandatory_param('view_name');
 check_mandatory_param('view_type');
 
-//Gather Device ID (numeric) from procvided ID
-preg_match("/\S*?(?<device_id>\d+?)$/", $context['ipam_device_id'], $matches);
-$context['ipam_device_id'] = $matches['device_id'];
-
 $view_type = $context["view_type"];
 if (isset($context['ipam_device_id'])) {
-	$ipam_device_id = $context['ipam_device_id'];
+        //Gather Device ID (numeric) from procvided ID
+        $ipam_device_id = getIdFromUbiId($context['ipam_device_id']);
 }
 
 $pos = strpos($view_type, "..");
@@ -28,7 +25,7 @@ if($pos === false) {
 	$topo_script = '/opt/fmc_repository/Process/Topology/Topology_Types/' . $view_type . '.php';
 	logToFile("using topology script: ".$topo_script."\n");
   	require_once $topo_script;
-    	if ($view_type == 'VRF') {
+    	if (($view_type == 'VRF') && isset($ipam_device_id)) {
     		$res = topology_create_service_view($ipam_device_id);
    	} else {
 		$res =  topology_create_view();
