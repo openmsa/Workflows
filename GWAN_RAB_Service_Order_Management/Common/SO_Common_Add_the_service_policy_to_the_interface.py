c3cd31ac46742b5dd410744bc9a157ebb4ac7b31
Sergio Lebon <sle@ubiqube.com>
2021-06-16 17:49:19 +0200

    Skip handling empty service configuration in SO Management WF.

--- GWAN_RAB_Service_Order_Management/Common/SO_Common_Add_the_service_policy_to_the_interface.py
+++ GWAN_RAB_Service_Order_Management/Common/SO_Common_Add_the_service_policy_to_the_interface.py
@@ -47,59 +47,62 @@ def get_process_instance(orch, process_id, timeout = 600, interval=5):
 #                                                  #
 ####################################################
 
-#Get device id (router) from context (e.g: UBI2455).
-device_ref = context['device_external_ref']
-#device_ref = context['device_id']
-device_id = device_ref[3:]
+#Get ServicePolicy dictionary object from context.
+if 'ServicePolicy' in context:
 
-#Get StaticRouting dictionary object from context.
-service_policy_list = context['ServicePolicy']
+    #Get device id (router) from context (e.g: UBI2455).
+    device_ref = context['device_external_ref']
+    #device_ref = context['device_id']
+    device_id = device_ref[3:]
 
-#Initiate orchestraction object.
-ubiqube_id = context['UBIQUBEID']
-orch = Orchestration(ubiqube_id)
+    #Get StaticRouting dictionary object from context.
+    service_policy_list = context['ServicePolicy']
 
-#Static Routing Management WF service name constant variable.
-SERVICE_NAME = 'Process/nttcw-gwan-rab-wf/Service_Policy_Management/Service_Policy_Management'
-CREATE_PROCESS_NAME = 'New_Service'
-ADD_PROCESS_NAME = 'Add_Service_Policy'
-service_id = ''
-service_ext_ref = ''
-#Instantiate new Static_Routing_Management WF dedicated for the device_id.
-if not 'service_policy_service_instance' in context:
-    data = dict(device_id=device_ref, SO_service_instance_id=context['SERVICEINSTANCEID'], SO_service_external_ref=context['SERVICEINSTANCEREFERENCE'])
-    orch.execute_service(SERVICE_NAME, CREATE_PROCESS_NAME, data)
-    response = json.loads(orch.content)
-    context['response'] = response
-    status = response.get('status').get('status')
-    if status == constants.ENDED:
-        if 'serviceId' in response:
-            service_id = response.get('serviceId').get('id')
-            service_ext_ref = response.get('serviceId').get('serviceExternalReference')
-            #Store service_instance_id of Static_Routing_Management WF in context.
-            context['service_policy_service_instance'] = dict(external_ref=service_ext_ref, instance_id=service_id)
-        else:
-            MSA_API.task_error('Missing service id return by orchestration operation, (#' + str(service_id) + ')', context, True) 
-    else:
-        MSA_API.task_error('Execute service operation failed, (#' + str(service_id) + ')', context, True)
-#Update service_instance external reference to "SERVICE_POLICY_" + device_ext_ref (e.g: SERVICE_POLICY_UBI2455).
-#service_ext_ref = 'SERVICE_POLICY_' + device_ext_ref
+    #Initiate orchestraction object.
+    ubiqube_id = context['UBIQUBEID']
+    orch = Orchestration(ubiqube_id)
 
-#Loop in StaticRouting dictionary object by calling the Static_Routing_Management process 'Add Static routing'.
-for sp in service_policy_list:
-    data = dict(interface_name=sp['interface_name'], direction=sp['direction'], policy_name=sp['policy_name'], SO_service_instance_id=context['SERVICEINSTANCEID'], SO_service_external_ref=context['SERVICEINSTANCEREFERENCE'])  
-    if isinstance(data, dict):
-        service_ext_ref = context.get('service_policy_service_instance').get('external_ref')
-        #execute service by ref.
-        orch.execute_service_by_reference(ubiqube_id, service_ext_ref, SERVICE_NAME, ADD_PROCESS_NAME, data)
+    #Service_Policy Management WF service name constant variable.
+    SERVICE_NAME = 'Process/nttcw-gwan-rab-wf/Service_Policy_Management/Service_Policy_Management'
+    CREATE_PROCESS_NAME = 'New_Service'
+    ADD_PROCESS_NAME = 'Add_Service_Policy'
+    service_id = ''
+    service_ext_ref = ''
+    #Instantiate new Service_Policy_Management WF dedicated for the device_id.
+    if not 'service_policy_service_instance' in context:
+        data = dict(device_id=device_ref, SO_service_instance_id=context['SERVICEINSTANCEID'], SO_service_external_ref=context['SERVICEINSTANCEREFERENCE'])
+        orch.execute_service(SERVICE_NAME, CREATE_PROCESS_NAME, data)
         response = json.loads(orch.content)
-        service_id = response.get('serviceId').get('id')
-        process_id = response.get('processId').get('id')
-        #get service process details.
-        response = get_process_instance(orch, process_id)
+        context['response'] = response
         status = response.get('status').get('status')
-        details = response.get('status').get('details')
-        if status == constants.FAILED:
-            MSA_API.task_error('Execute service operation is failed: ' + details + ' (#' + str(service_id) + ')', context, True) 
+        if status == constants.ENDED:
+            if 'serviceId' in response:
+                service_id = response.get('serviceId').get('id')
+                service_ext_ref = response.get('serviceId').get('serviceExternalReference')
+                #Store service_instance_id of Static_Routing_Management WF in context.
+                context['service_policy_service_instance'] = dict(external_ref=service_ext_ref, instance_id=service_id)
+            else:
+                MSA_API.task_error('Missing service id return by orchestration operation, (#' + str(service_id) + ')', context, True) 
+        else:
+            MSA_API.task_error('Execute service operation failed, (#' + str(service_id) + ')', context, True)
 
-MSA_API.task_success( 'Service Policy added successfully to the device ' + device_ref + ' (#' + str(service_id) + ')', context, True)
+    #Loop in StaticRouting dictionary object by calling the Service_Policy_Management process 'Add Service Policy'.
+    for sp in service_policy_list:
+        data = dict(interface_name=sp['interface_name'], direction=sp['direction'], policy_name=sp['policy_name'], SO_service_instance_id=context['SERVICEINSTANCEID'], SO_service_external_ref=context['SERVICEINSTANCEREFERENCE'])  
+        if isinstance(data, dict):
+            service_ext_ref = context.get('service_policy_service_instance').get('external_ref')
+            #execute service by ref.
+            orch.execute_service_by_reference(ubiqube_id, service_ext_ref, SERVICE_NAME, ADD_PROCESS_NAME, data)
+            response = json.loads(orch.content)
+            service_id = response.get('serviceId').get('id')
+            process_id = response.get('processId').get('id')
+            #get service process details.
+            response = get_process_instance(orch, process_id)
+            status = response.get('status').get('status')
+            details = response.get('status').get('details')
+            if status == constants.FAILED:
+                MSA_API.task_error('Execute service operation is failed: ' + details + ' (#' + str(service_id) + ')', context, True) 
+
+    MSA_API.task_success( 'Service Policy added successfully to the device ' + device_ref + ' (#' + str(service_id) + ')', context, True)
+    
+MSA_API.task_success('No Service Policy to be added in the device.', context , True)
--- GWAN_RAB_Service_Order_Management/SO_Delete/SO_Delete_Delete_ACL.py
+++ GWAN_RAB_Service_Order_Management/SO_Delete/SO_Delete_Delete_ACL.py
@@ -73,73 +73,77 @@ def get_process_instance(orch, process_id, timeout = 600, interval=5):
 #                                                  #
 ####################################################
 
-#Get device id (router) from context (e.g: UBI2455).
-device_ref = context['device_external_ref']
-device_id = device_ref[3:]
+if 'ACL' in context:
 
-#Get StaticRouting dictionary object from context.
-acl_dicts = context['ACL']
+    #Get device id (router) from context (e.g: UBI2455).
+    device_ref = context['device_external_ref']
+    device_id = device_ref[3:]
 
-#Initiate orchestraction object.
-ubiqube_id = context['UBIQUBEID']
-orch = Orchestration(ubiqube_id)
+    #Get StaticRouting dictionary object from context.
+    acl_dicts = context['ACL']
 
-#Static Routing Management WF service name constant variable.
-SERVICE_NAME = 'Process/nttcw-gwan-rab-wf/Access_List_Management/Access_List_Management'
-CREATE_PROCESS_NAME = 'New_Service'
-ADD_PROCESS_NAME = 'Delete_ACL'
-service_id = ''
-service_ext_ref = ''
+    #Initiate orchestraction object.
+    ubiqube_id = context['UBIQUBEID']
+    orch = Orchestration(ubiqube_id)
 
-#Instantiate new Static_Routing_Management WF dedicated for the device_id.
-if not 'acl_service_instance' in context:
-    data = dict(device_id=device_ref, SO_service_instance_id=context['SERVICEINSTANCEID'], SO_service_external_ref=context['SERVICEINSTANCEREFERENCE'])
-    orch.execute_service(SERVICE_NAME, CREATE_PROCESS_NAME, data)
-    response = json.loads(orch.content)
-    context['response'] = response
-    status = response.get('status').get('status')
-    if status == constants.ENDED:
-        if 'serviceId' in response:
-            service_id = response.get('serviceId').get('id')
-            service_ext_ref = response.get('serviceId').get('serviceExternalReference')
-            #Store service_instance_id of Static_Routing_Management WF in context.
-            context['acl_service_instance'] = dict(external_ref=service_ext_ref, instance_id=service_id)
+    #Static Routing Management WF service name constant variable.
+    SERVICE_NAME = 'Process/nttcw-gwan-rab-wf/Access_List_Management/Access_List_Management'
+    CREATE_PROCESS_NAME = 'New_Service'
+    ADD_PROCESS_NAME = 'Delete_ACL'
+    service_id = ''
+    service_ext_ref = ''
+
+    #Instantiate new Static_Routing_Management WF dedicated for the device_id.
+    if not 'acl_service_instance' in context:
+        data = dict(device_id=device_ref, SO_service_instance_id=context['SERVICEINSTANCEID'], SO_service_external_ref=context['SERVICEINSTANCEREFERENCE'])
+        orch.execute_service(SERVICE_NAME, CREATE_PROCESS_NAME, data)
+        response = json.loads(orch.content)
+        context['response'] = response
+        status = response.get('status').get('status')
+        if status == constants.ENDED:
+            if 'serviceId' in response:
+                service_id = response.get('serviceId').get('id')
+                service_ext_ref = response.get('serviceId').get('serviceExternalReference')
+                #Store service_instance_id of Static_Routing_Management WF in context.
+                context['acl_service_instance'] = dict(external_ref=service_ext_ref, instance_id=service_id)
+            else:
+                MSA_API.task_error( 'Missing service id return by orchestration operation.', context, True)
         else:
-            MSA_API.task_error( 'Missing service id return by orchestration operation.', context, True)
-    else:
-        MSA_API.task_error('Execute service operation failed.', context, True)
-#Update service_instance external reference to "ACL_" + device_ext_ref (e.g: ACL_UBI2455).
-#service_ext_ref = 'ACL_' + device_ext_ref
+            MSA_API.task_error('Execute service operation failed.', context, True)
+    #Update service_instance external reference to "ACL_" + device_ext_ref (e.g: ACL_UBI2455).
+    #service_ext_ref = 'ACL_' + device_ext_ref
 
-#Loop in acl dictionaries and in acl list by calling the Access_List_Management process 'Add_ACL'.
-data = dict(SO_service_instance_id=context['SERVICEINSTANCEID'], SO_service_external_ref=context['SERVICEINSTANCEREFERENCE'])
-for key, acl_list  in acl_dicts.items():
-    acl_name = ''
-    #ensure acl_list is not empty otherwise break the loop.
-    if len(acl_list):
-        count = 0
-        #loop in acl list.
-        for acl in acl_list:
-            if isinstance(acl, dict):
-                if count == 0:
-                    acl_name = get_config_param_val(context, acl, 'acl_name')
-                count +=1
-        #prepare data dict.
-        data['acl_name'] = acl_name
-        #execute 'Access_List_Management' process 'Add_ACL'.
-        if isinstance(data, dict) and acl_name:
-            service_ext_ref = context.get('acl_service_instance').get('external_ref')
-            #execute service by ref.
-            orch.execute_service_by_reference(ubiqube_id, service_ext_ref, SERVICE_NAME, ADD_PROCESS_NAME, data)
-            response = json.loads(orch.content)
-            service_id = response.get('serviceId').get('id')
-            process_id = response.get('processId').get('id')
-            #get service process details.
-            response = get_process_instance(orch, process_id)
-            status = response.get('status').get('status')
-            details = response.get('status').get('details')
-            if status == constants.FAILED:
-                MSA_API.task_error('Execute service operation is failed: ' + details + ' (#' + str(service_id) + ')', context, True)
-              
-    
-MSA_API.task_success('Access-list deleted successfully to the device ' + device_ref + ' (#' + str(service_id) + ')', context, True)
+    #Loop in acl dictionaries and in acl list by calling the Access_List_Management process 'Add_ACL'.
+    data = dict(SO_service_instance_id=context['SERVICEINSTANCEID'], SO_service_external_ref=context['SERVICEINSTANCEREFERENCE'])
+    for key, acl_list  in acl_dicts.items():
+        acl_name = ''
+        #ensure acl_list is not empty otherwise break the loop.
+        if len(acl_list):
+            count = 0
+            #loop in acl list.
+            for acl in acl_list:
+                if isinstance(acl, dict):
+                    if count == 0:
+                        acl_name = get_config_param_val(context, acl, 'acl_name')
+                    count +=1
+            #prepare data dict.
+            data['acl_name'] = acl_name
+            #execute 'Access_List_Management' process 'Add_ACL'.
+            if isinstance(data, dict) and acl_name:
+                service_ext_ref = context.get('acl_service_instance').get('external_ref')
+                #execute service by ref.
+                orch.execute_service_by_reference(ubiqube_id, service_ext_ref, SERVICE_NAME, ADD_PROCESS_NAME, data)
+                response = json.loads(orch.content)
+                service_id = response.get('serviceId').get('id')
+                process_id = response.get('processId').get('id')
+                #get service process details.
+                response = get_process_instance(orch, process_id)
+                status = response.get('status').get('status')
+                details = response.get('status').get('details')
+                if status == constants.FAILED:
+                    MSA_API.task_error('Execute service operation is failed: ' + details + ' (#' + str(service_id) + ')', context, True)
+                  
+        
+    MSA_API.task_success('Access-list deleted successfully to the device ' + device_ref + ' (#' + str(service_id) + ')', context, True)
+
+MSA_API.task_success('No Access-list to be deleted in the device.', context, True)
