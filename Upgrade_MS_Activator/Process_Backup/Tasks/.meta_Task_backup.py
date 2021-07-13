5cfed8dff4c981835f71b496eec5a7f357d37386
Amit Raj <ara@ubiqube.com>
2021-07-13 09:46:42 +0100

    With all meta files

--- Upgrade_MS_Activator/Process_Backup/Tasks/.meta_Task_backup.py
+++ Upgrade_MS_Activator/Process_Backup/Tasks/.meta_Task_backup.py
@@ -3,11 +3,11 @@
     <map>
         <entry>
             <key>DISPLAYNAME</key>
-            <value>Upgrade_MS_Activator.xml</value>
+            <value>Task_backup.py</value>
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1626102650468</value>
+            <value>1626102551267</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,11 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1626102650463</value>
-        </entry>
-        <entry>
-            <key>MODEL</key>
-            <value>0</value>
+            <value>1626102551263</value>
         </entry>
         <entry>
             <key>TAG</key>
@@ -29,12 +25,8 @@
             <value>text</value>
         </entry>
         <entry>
-            <key>MANUFACTURER</key>
-            <value>0</value>
-        </entry>
-        <entry>
             <key>TYPE</key>
-            <value>FILE</value>
+            <value>UPLOAD</value>
         </entry>
         <entry>
             <key>COMMENT</key>
