95dba06f8a1b36ef9b1ea7522078b42aee54e0b8
root <devops@openmsa.co>
2021-08-23 16:18:30 +0000

    device_id fixed, hardcoded ip/port fixed, non-strict-ssl added

--- ETSI/Python/_py__NFVI_Resources_Management/Process_Delete_VIM/Tasks/.meta_Task_Delete_VIM_Resources.py
+++ ETSI/Python/_py__NFVI_Resources_Management/Process_Delete_VIM/Tasks/.meta_Task_Delete_VIM_Resources.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1629459738420</value>
+            <value>1629735343381</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1629459738418</value>
+            <value>1629735343376</value>
         </entry>
         <entry>
             <key>TAG</key>
--- ETSI/Python/_py__NFVI_Resources_Management/Process_New_Service/Tasks/Task_Sync_to_NFVO.py
+++ ETSI/Python/_py__NFVI_Resources_Management/Process_New_Service/Tasks/Task_Sync_to_NFVO.py
@@ -1,21 +1,32 @@
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 from msa_sdk.order import Order
+from msa_sdk.device import Device
+
 
 if __name__ == "__main__":
 
     dev_var = Variables()
-    dev_var.add('device_id', var_type='Device')
-    dev_var.add('mano_user', var_type='String')
-    dev_var.add('mano_pass', var_type='Password')
+    dev_var.add('nfvo_device', var_type='Device')
+    dev_var.add('vnfm_device', var_type='Device')
     context = Variables.task_call(dev_var)
 
-    device_short_id = context['device_id'][3:]
+    mano_me_id = context["nfvo_device"][3:]
+    mano_ip    = Device(device_id=mano_me_id).management_address
+    mano_var   = Device(device_id=mano_me_id).get_configuration_variable("HTTP_PORT")
+    mano_port  = mano_var.get("value")
+    mano_user  = Device(device_id=mano_me_id).login
+    mano_pass  = Device(device_id=mano_me_id).password
+    
+    context["mano_ip"]   = mano_ip
+    context["mano_port"] = mano_port
+    context["mano_user"] = mano_user
+    context["mano_pass"] = mano_pass
 
-    order = Order(str(device_short_id))
+    order = Order(str(mano_me_id))
     order.command_synchronize(timeout=60)
 
     ret = MSA_API.process_content('ENDED',
-        f'Device {context["device_id"]} synchronized', context, True)
+        f'Device {context["nfvo_device"]} synchronized', context, True)
 
     print(ret)
