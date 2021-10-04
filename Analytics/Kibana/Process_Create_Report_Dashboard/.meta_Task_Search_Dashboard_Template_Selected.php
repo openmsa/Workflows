0267c3f9d81223fa8cc4ed9378d630e3f451182c
dma <dma@ubiqube.com>
2021-10-04 21:30:19 +0100

    MSA-10091 - kibana live filtering mechanism

--- Analytics/Kibana/Process_Create_Report_Dashboard/.meta_Task_Search_Dashboard_Template_Selected.php
+++ Analytics/Kibana/Process_Create_Report_Dashboard/.meta_Task_Search_Dashboard_Template_Selected.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1618372823705</value>
+            <value>1633377838235</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
--- Analytics/Kibana/Process_Update_Dashboard/Task_Search_Dashboard_Selected_for_Updating.php
+++ Analytics/Kibana/Process_Update_Dashboard/Task_Search_Dashboard_Selected_for_Updating.php
@@ -4,121 +4,14 @@ require_once '/opt/fmc_repository/Process/Reference/Common/common.php';
 // List all the parameters required by the task
 function list_args()
 {
-create_var_def('template name', 'String');
+create_var_def('template_id', 'String');
 create_var_def('dashboardName', 'String');
-
-
-
- create_var_def('filterType', 'String');
-
- create_var_def('advancedFilter', 'Composite');
- create_var_def('advancedFilterEmpty', 'String');
-
-
- create_var_def('basicFilterEmpty', 'String');
- // create_var_def('basicFilter', 'Composite');
-
-
- create_var_def('filter.0.field', 'Composite');
- create_var_def('filter.0.apply', 'Composite');
- create_var_def('filter.0.value', 'Composite');
-
- create_var_def('filter.0.operator', 'Composite');
- create_var_def('filter.0.resultBasictFilter', 'String');
-
-
-create_var_def('levelView', 'String');
-
-create_var_def('device_id', 'Device');
-
-create_var_def('device_ip', 'IpAddress');
-
-
-}
-// ENTER YOUR CODE HERE
-// Update $context array for output variables
-
-
-if ($context['filterType']=='Advanced'){
-	
-	if(!empty($context['advancedFilter'])){
-	$context['advancedFilterEmpty']='false';
-	}
-	else{
-
-	$context['advancedFilterEmpty']='true';
-	}
-
+create_var_def('Hash', 'String');
+create_var_def('kibanaUrl', 'String');
 }
 
-else {
-	
-	if(empty($context['filter']))
-	{
-	  $context['basicFilter'] ='';
-	}
-
-	
-	else {
-
-	foreach ($context['filter'] as $filter) {
-		
-			if($filter['apply']=="true"){
-				
-          			$filter['resultBasicFilter']=' '.$filter['operator'].' '.$filter['field'].':'.$filter['value'];
-				
-				$context['basicFilter'].=$filter['resultBasicFilter'];
-			}
-				
-		
-	}
-	
-	
-	$context['basicFilter'] = substr($context['basicFilter'], 3);
-	
-	}
-
-	
-	if(!empty($context['basicFilter'])){
-	$context['basicFilterEmpty']='false';
-	}
-	else{
-
-	$context['basicFilterEmpty']='true';
-	}
-}
-
-
-/* to recover device ip address */
-
-if($context['levelView']=='device') {
-
-preg_match('/(?<digit>\d+)/',$context['device_id'],$device_id_number);
-$context['device_id_number']=$device_id_number[1];
-$context['cmd']="/opt/ubi-jentreprise/bin/api/device/readDeviceById.sh ".$context['device_id_number'];
-$response_cmd= shell_exec($context['cmd']);
-preg_match('#<ipAddress>\n<address>(.*)</address>\n<mask>#',$response_cmd, $out);
-$context['device_ip']=$out[1];
-
-}
-
-
-
-
-/* Recover IP Address for Kibana */
-
-//define('HOST_KIBANA', 'UBI_ES_KIBANA_IP');
-//$_KIBANA=get_vars_value(HOST_KIBANA);
-//preg_match('#(.*):#',$_KIBANA,$_KIBANA);
 $context['kibanaIpAddress']="msa_kibana";
-
-
-/* Recover Port Number for Kibana */
-
-//$_KIBANA=get_vars_value(HOST_KIBANA);
-//preg_match('#:(.*)#',$_KIBANA,$_KIBANA);
 $context['kibanaPort']="5601";
-
 
 /**
 curl -XPOST "10.30.18.116:5601/kibana/api/saved_objects/_export" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'
@@ -133,39 +26,25 @@ curl -XPOST "10.30.18.116:5601/kibana/api/saved_objects/_export" -H 'kbn-xsrf: t
 '
 **/
 
-/* ***   Recove ES ip address   **** */ 
-//define('HOST_ES', 'UBI_ES_WEBPORTAL_ENDPOINT');
-//$_H=get_vars_value(HOST_ES);
 $context['ipAddress']="msa_kibana";
-
 /* ** Set basePath ** */
 $context['basePath']="/kibana";
-
-/* ** recove customer_id) ** */
-preg_match('/(?<digit>\d+)/',$context['UBIQUBEID'],$matches);
-$context['customer_id']=$matches[1];
-
-
-
-
 /* ** Url and Method to Find selected Dashboard **** */ 
-$context['searchingURI']='http://'.$context['ipAddress'].':'.$context['port'].$context['basePath'].'/api/saved_objects/_export';
+$context['searchingURI']='http://'.$context['ipAddress'].':'.$context['kibanaPort'].$context['basePath'].'/api/saved_objects/_export';
+$context['Hash']=$context['template_id'];
+$context['kibanaUrl']='http://'.$context['kibanaIpAddress'].':'.$context['kibanaPort'].'/app/kibana#/dashboard/'.$context['Hash'].'/';
 
 $body_request='
 {
   "objects": [
     {
       "type": "'.$context['type'].'",
-      "id": "'.$context['template name'].'"
+      "id": "'.$context['template_id'].'"
     }
-  ]
+  ],
+  "excludeExportDetails": true
 }
 ';
-
-
-
-$context['f']='/tmp/file_kibana_'.$context['Hash'].'.ndjson';
-$fp = fopen($context['f'], "w");
 
 $ci = curl_init();
     curl_setopt($ci, CURLOPT_URL,$context['searchingURI']);
@@ -176,27 +55,18 @@ $ci = curl_init();
     curl_setopt($ci, CURLOPT_POSTFIELDS, $body_request);
     curl_setopt($ci, CURLOPT_HTTPHEADER, array('kbn-xsrf: true','Content-Type: application/json','Authorization: Basic c3VwZXJ1c2VyOnheWnl1R002fnU9K2ZZMkc='));
     $result= curl_exec($ci);
-    fwrite($fp, $result);
 
 /* Store the result in a variable      */    
-$context['result']=$result;	
-
-/* ** Store Kibana  Url  ***/ 
-
-$context['kibanaUrl']='http://'.$context['kibanaIpAddress'].':'.$context['kibanaPort'].'/app/kibana#/dashboard/'.$context['Hash'].'/';
+$context['result']=$result;
 
 $check=json_decode($result,true);
 if(!isset($check['statusCode'])){
- 	
-	 task_exit(ENDED, "Template ID found");
+
+    task_exit(ENDED, 'Template ID '.$context['template_id'].' has been found');
+	 
 }
-
- else{
- 	
-	 task_exit(WARNING, 'Template ID not found => '.$result.'');
+else{
+    task_exit(WARNING, 'Template ID '.$context['template_id'].' has not been found => '.$result.'');
 }
-
-
 //task_exit(ENDED, $result);
-
 ?>
