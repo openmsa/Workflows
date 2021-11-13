7ad095dc88c14e73be83c7d22948a068ff9cfc55
ncroot <>
2021-11-13 17:21:19 +0000

    Updating Workflow on  - 13-11-2021 17:21:19 - ncroot

--- Connectivity_Test/Process_Create_connectivity_test/Tasks/.meta_Task_register_addresses.py
+++ Connectivity_Test/Process_Create_connectivity_test/Tasks/.meta_Task_register_addresses.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1636824079496</value>
+            <value>1636824079687</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1636824079493</value>
+            <value>1636824079684</value>
         </entry>
         <entry>
             <key>TAG</key>
