0815e36d245c964433d5ca2672fa1124250df6b8
ncroot <>
2021-11-14 15:05:04 +0000

    Updating Workflow on  - 14-11-2021 15:05:04 - admin1

--- test_isdev/Process_isdev_pro/Tasks/.meta_Task_isdev.py
+++ test_isdev/Process_isdev_pro/Tasks/.meta_Task_isdev.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1636902275163</value>
+            <value>1636902304513</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1636902275160</value>
+            <value>1636902304510</value>
         </entry>
         <entry>
             <key>TAG</key>
