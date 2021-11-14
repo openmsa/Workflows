8843ef4510688062d8b4f7b4b4b045129094e524
ncroot <>
2021-11-14 14:56:49 +0000

    Updating Workflow on  - 14-11-2021 14:56:49 - admin1

--- test_isdev/Process_isdev_pro/Tasks/.meta_Task_isdev.py
+++ test_isdev/Process_isdev_pro/Tasks/.meta_Task_isdev.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1636901217447</value>
+            <value>1636901809894</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1636901217444</value>
+            <value>1636901809891</value>
         </entry>
         <entry>
             <key>TAG</key>
--- test_isdev/Process_isdev_pro/Tasks/Task_isdev.py
+++ test_isdev/Process_isdev_pro/Tasks/Task_isdev.py
@@ -3,52 +3,42 @@ Visit http://[YOUR_MSA_URL]/msa_sdk/ to see what you can import.
 '''
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
-
-'''
-List all the parameters required by the task
-
-You can use var_name convention for your variables
-They will display automaticaly as "Var Name"
-The allowed types are:
-  'String', 'Boolean', 'Integer', 'Password', 'IpAddress',
-  'IpMask', 'Ipv6Address', 'Composite', 'OBMFRef', 'Device'
-
- Add as many variables as needed
+from msa_sdk.device import Device
+from msa_sdk import util
+import json
 '''
 dev_var = Variables()
 dev_var.add('var_name', var_type='String')
 dev_var.add('var_name2', var_type='Integer')
-
-'''
-context => Service Context variable per Service Instance
-All the user-inputs of Tasks are automatically stored in context
-Also, any new variables should be stored in context which are used across Service Instance
-The variables stored in context can be used across all the Tasks and Processes of a particular Service
-Update context array [add/update/delete variables] as per requirement
-
-ENTER YOUR CODE HERE
-'''
 context = Variables.task_call(dev_var)
 context['var_name2'] = int(context['var_name2']) + 1
-
 '''
-Format of the Task response :
-JSON format : {"wo_status":"status","wo_comment":"comment","wo_newparams":{json_body}}
-wo_status : ENDED [Green color] or FAILED [Red color] or WARNING [Orange color]
-			-> While the Task is Running [means no response returned yet], task status is RUNNING [Blue color]
-         -> When status is returned as FAILED, the Orchestration Engine stops the Process Execution from this Task
-wo_comment : Appropriate Comment to display as per the success/failure of the Task
-wo_newparams : json_body parameters returned from this Task
 
-Function process_content() takes care of Creating a Json response from inputs
-This function definiton can be found at : http://[YOUR_MSA_URL]/msa_sdk/msa_api.html#msa_sdk.msa_api.MSA_API.process_content
-NOTE : For 'wo_newparams', always pass "context" [whether wo_status is ENDED/FAILED/WARNING to preserve it across Service Instance]
-    -> Last argument "true" mentions whether the json_response to be Logged in the logfile : /opt/jboss/latest/logs/process.log
-    -> If not passed, it's "false"
+dev_var = Variables()
+dev_var.add('addresses.0.ip')
+dev_var.add('addresses.0.status')
+dev_var.add('addresses.1.ip')
+dev_var.add('addresses.1.status')
+context = Variables.task_call(dev_var)
 
-The response "ret" should be echoed from the Task "print(ret)" which is read by Orchestration Engine
-In case of FAILURE/WARNING, the Task can be Terminated by calling "exit" as per Logic
-'''
+process_id = context['SERVICEINSTANCEID']
+device = Device()
+addresses = context['addresses']
+
+i=0
+for address in addresses:
+  ip = address['ip']
+
+  ping_result = device.ping(ip)
+  util.log_to_process_file(process_id, ping_result)
+  ping_result_json = json.loads(ping_result)
+  num = len(context['addresses'])
+  context['addresses'][i] = {}
+  context['addresses'][i]['ip'] = ip
+  context['addresses'][i]['status'] = ping_result_json['status']
+  i += 1
+
+
 ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
 print(ret)
 
