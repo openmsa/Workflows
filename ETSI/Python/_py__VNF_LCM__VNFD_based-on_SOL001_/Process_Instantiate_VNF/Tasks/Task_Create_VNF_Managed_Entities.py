925a68ba75ff33c5c030c7d2c94acbd10b315452
Sergio Lebon <sle@ubiqube.com>
2022-01-26 22:48:40 +0100

    Get VNFC Resources instance external IP address from VIM.

--- ETSI/Python/_py__VNF_LCM__VNFD_based-on_SOL001_/Process_Instantiate_VNF/Tasks/Task_Create_VNF_Managed_Entities.py
+++ ETSI/Python/_py__VNF_LCM__VNFD_based-on_SOL001_/Process_Instantiate_VNF/Tasks/Task_Create_VNF_Managed_Entities.py
@@ -1,11 +1,98 @@
 import json
+import time
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 from msa_sdk.device import Device
 from msa_sdk import constants
+import openstack
 
+from custom.ETSI.NfviVim import NfviVim
 from custom.ETSI.VnfLcmSol003 import VnfLcmSol003
 from custom.ETSI.VnfPkgSol005 import VnfPkgSol005
+
+
+'''
+Get VIM connection.
+'''
+def _get_vim_connection_auth(nfvo_device, vim_id):
+    #Openstack Authification Connection.
+    conn = ''
+    
+    #NFVO Info.
+    nfvo_mano_me_id = context["nfvo_device"][3:]
+    nfvo_mano_ip    = Device(device_id=nfvo_mano_me_id).management_address
+    nfvo_mano_var   = Device(device_id=nfvo_mano_me_id).get_configuration_variable("HTTP_PORT")
+    nfvo_mano_port  = nfvo_mano_var.get("value")
+    nfvo_mano_user  = Device(device_id=nfvo_mano_me_id).login
+    nfvo_mano_pass  = Device(device_id=nfvo_mano_me_id).password
+    
+    nfviVim = NfviVim(nfvo_mano_ip, nfvo_mano_port)
+    nfviVim.set_parameters(nfvo_mano_user, nfvo_mano_pass)
+    
+    #Get VIM connection info by id.
+    vim_list = nfviVim.nfvi_vim_get()
+
+    if vim_list:
+        for index, vimInfo in enumerate(vim_list.json()):
+            if vimInfo.get('vimId') == vim_id:
+                
+                context.update(vimInfo=vimInfo)
+                
+                auth_url = vimInfo['interfaceInfo']['endpoint']
+                auth_url = auth_url[:-2]
+                username = vimInfo['accessInfo']['username']
+                password = vimInfo['accessInfo']['password']
+                project_id = vimInfo['accessInfo']['projectId']
+                user_domain_id = vimInfo['accessInfo']['userDomain']
+                
+                context.update(auth_url=auth_url)
+                context.update(password=password)
+                context.update(project_id=project_id)
+                context.update(user_domain_id=user_domain_id)
+                
+                #Get Openstack connection
+                auth = dict(auth_url=auth_url, username=username, password=password, project_id=project_id, user_domain_id=user_domain_id)
+                conn = openstack.connection.Connection(region_name='RegionOne', auth=auth, compute_api_version='2',identity_interface ='public')
+    return conn
+
+
+'''
+Get external network from VIM tenant.
+'''
+def _get_vim_external_network(conn):
+    external_network = ''
+    for network in conn.network.networks():
+        if network.get('is_router_external') == True:
+            external_network = network.get('name')
+    return external_network
+
+'''
+Get VNFC resource (VDU) instance public IP address.
+'''
+def _get_vnfc_resource_public_ip_address(nfvo_device, vim_id, server_id):
+    
+    server_ip_addr = ''
+    
+    #Get openstack authenfication
+    conn = _get_vim_connection_auth(nfvo_device, vim_id)
+    
+    #Get openstack tenant external networks.
+    external_network = _get_vim_external_network(conn)
+    
+    #Get VDU (server instance) details.
+    servers = conn.compute.servers()
+    for server in servers:
+        if server.id == server_id:
+            addresses = server.addresses
+            
+            time.sleep(10)
+            for index, address in enumerate(addresses.get(external_network)):
+                if address.get('addr'):
+                    server_ip_addr = address.get('addr')
+                    break
+                                        
+    return server_ip_addr
+
 
 dev_var = Variables()
 context = Variables.task_call(dev_var)
@@ -33,9 +120,10 @@ if __name__ == "__main__":
     vnf_me_list = list()
     
     #For each VNFC-VDU create corresponding ME's.
-    for index, item in enumerate(vnfResourcesList):
+    for index, vnfR in enumerate(vnfResourcesList):
         #openstack server instance ID.
-        vnfResourceId = vnfResourcesList[index]["computeResource"]["resourceId"]
+        vnfResourceId = vnfR["computeResource"]["resourceId"]
+        vim_connection_id = vnfR["computeResource"]['vimConnectionId']
     
         ## get VDU details (@IP, Hostname).
         #TODO: create VIM SDK to call Openstack API servers resources.
@@ -43,11 +131,15 @@ if __name__ == "__main__":
         #Customer ID
         customer_id = subtenant_ext_ref[4:]
         #Kubernetes_generic manufacturer_id
-        manufacturer_id='20060101'
+        manufacturer_id='14020601'
         #Kubernetes_generic model_id
-        model_id='20060101'
+        model_id='14020601'
         #default IP address
-        management_address='1.1.1.1'
+        nfvo_device_ref = context.get('nfvo_device')
+        management_address = _get_vnfc_resource_public_ip_address(nfvo_device_ref, vim_connection_id, vnfResourceId)
+        if not management_address:
+            management_address = '1.1.1.1'
+        
         #Kubernetes adaptor does not use the password and login of ME.
         password = 'fake38passwOrd'
         management_port='22'
