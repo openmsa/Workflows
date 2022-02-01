0096523a88d8faf28a330b0f6d83d8f08e4581d2
Sergio Lebon <sle@ubiqube.com>
2022-02-01 10:14:38 +0100

    fix 'interfaceInfo' object typo.

--- ETSI/Python/_py__NFVI_Resources_Management/Process_Register_VIM/Tasks/.meta_Task_Register_VIM_Resources.py
+++ ETSI/Python/_py__NFVI_Resources_Management/Process_Register_VIM/Tasks/.meta_Task_Register_VIM_Resources.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1643151755206</value>
+            <value>1643185989760</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1643151755204</value>
+            <value>1643185989748</value>
         </entry>
         <entry>
             <key>TAG</key>
