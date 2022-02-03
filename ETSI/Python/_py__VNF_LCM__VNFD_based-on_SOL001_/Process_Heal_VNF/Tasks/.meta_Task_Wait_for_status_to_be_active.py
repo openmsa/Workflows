f4572b9e9cf5da292f7ed26314b04403f549cf56
root <devops@openmsa.co>
2022-02-03 15:59:40 +0000

    Added processes for healing vnf and live-migration of vnf instance

--- ETSI/Python/_py__VNF_LCM__VNFD_based-on_SOL001_/Process_Heal_VNF/Tasks/.meta_Task_Wait_for_status_to_be_active.py
+++ ETSI/Python/_py__VNF_LCM__VNFD_based-on_SOL001_/Process_Heal_VNF/Tasks/.meta_Task_Wait_for_status_to_be_active.py
@@ -3,11 +3,11 @@
     <map>
         <entry>
             <key>DISPLAYNAME</key>
-            <value>_py__VNF_LCM__VNFD_based-on_SOL001_.xml</value>
+            <value>Task_Wait_for_status_to_be_active.py</value>
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1643903674320</value>
+            <value>1643903670260</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,26 +15,18 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1643903674309</value>
-        </entry>
-        <entry>
-            <key>MODEL</key>
-            <value>0</value>
+            <value>1643903670257</value>
         </entry>
         <entry>
             <key>TAG</key>
         </entry>
         <entry>
             <key>FILE_TYPE</key>
-            <value>unknown</value>
-        </entry>
-        <entry>
-            <key>MANUFACTURER</key>
-            <value>0</value>
+            <value>text</value>
         </entry>
         <entry>
             <key>TYPE</key>
-            <value>FILE</value>
+            <value>UPLOAD</value>
         </entry>
         <entry>
             <key>COMMENT</key>
