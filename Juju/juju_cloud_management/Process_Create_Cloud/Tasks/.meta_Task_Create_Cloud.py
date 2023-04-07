fce6da9d9a83b6f3505b1c53f84b6c6cb38a9fe5
Sergio Lebon <sle@ubiqube.com>
2023-04-07 14:31:29 +0200

    Init Juju workflows.

---
+++ Juju/juju_cloud_management/Process_Create_Cloud/Tasks/.meta_Task_Create_Cloud.py
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<metadata>
+    <map>
+        <entry>
+            <key>DISPLAYNAME</key>
+            <value>Task_Create_Cloud.py</value>
+        </entry>
+        <entry>
+            <key>DATE_MODIFICATION</key>
+            <value>1680618730314</value>
+        </entry>
+        <entry>
+            <key>REPOSITORY</key>
+            <value>Process</value>
+        </entry>
+        <entry>
+            <key>DATE_CREATION</key>
+            <value>1680618730311</value>
+        </entry>
+        <entry>
+            <key>TAG</key>
+        </entry>
+        <entry>
+            <key>FILE_TYPE</key>
+            <value>text</value>
+        </entry>
+        <entry>
+            <key>TYPE</key>
+            <value>UPLOAD</value>
+        </entry>
+        <entry>
+            <key>COMMENT</key>
+        </entry>
+    </map>
+</metadata>
