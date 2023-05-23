fa50642476d99a576761adb12f1bc08d79589ecd
Antoine Brun <antoine.brun@gmail.com>
2023-05-23 09:57:21 +0200

    remove remaining ETSI files

--- ETSI/Python/_py__NS_LCM__NSD_based-on_SOL001_/Process_Instantiate_NS/Tasks/Task_Get_NS_Instance.py
+++
@@ -1,9 +0,0 @@
-from msa_sdk.variables import Variables
-from msa_sdk.msa_api import MSA_API
-
-from custom.ETSI.NsLcmOpOccsSol005 import NsLcmOpOccsSol005
-
-
-if __name__ == "__main__":
-    
-    pass
--- ETSI/Python/_py__VNF___NS_Packages_Management/Process_Delete_NS_Package/Tasks/Task_Synchronize.py
+++
@@ -1,9 +0,0 @@
-from msa_sdk.variables import Variables
-from msa_sdk.msa_api import MSA_API
-
-if __name__ == "__main__":
-
-    context = Variables.task_call()
-    
-    ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
-    print(ret)
