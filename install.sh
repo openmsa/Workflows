37786430612efb1d3d407ad5eae3cbd1f7388076
Olivier Vignaud <ovi@ubiqube.com>
2023-10-27 14:43:11 +0200

    MSA-13292: Add install script.

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+. /usr/share/install-libraries/il-lib.sh
+
+pushd /opt/fmc_repository/Process || exit;
+emit_step "WF tutorials"
+to_link=(Tutorials BIOS_Automation Private_Cloud Ansible_integration Public_Cloud Manage_Device_Conf_Variables BIOS_Automation Upgrade_MSActivator)
+for tl in "${to_link[@]}"
+do
+    emit_step $tl
+    mk_wf_meta_link $tl
+done
+
+popd || exit
+
+
+
