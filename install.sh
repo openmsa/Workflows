deb08e8fb3a92fd3eb221d81edbd780061fafc28
Antoine Brun <antoine.brun@gmail.com>
2025-04-17 16:59:20 +0200

    remove symlinks of useless WF

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 . /usr/share/install-libraries/il-lib.sh
 
 pushd /opt/fmc_repository/Process || exit;
-emit_step "WF tutorials"
-to_link=(Tutorials BIOS_Automation Private_Cloud Ansible_integration Public_Cloud BIOS_Automation Upgrade_MSActivator)
+emit_step "Sample Workflows"
+to_link=(BIOS_Automation Private_Cloud Public_Cloud )
 for tl in "${to_link[@]}"
 do
     emit_step $tl
