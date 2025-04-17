0ed3b71e6a774740297badc26a3e867697ffd489
Antoine Brun <antoine.brun@gmail.com>
2025-04-17 12:41:05 +0200

    🪛 remove tutorials WF

--- Tutorials/Process_Wizard/README.md
+++
@@ -1,2 +0,0 @@
-Sample workflow that illustrate how to create process tasks that put themselves on pause and wait for user input 
-
--- Tutorials/python/Simple_Firewall/Delete_Service_delete_service.py
+++
@@ -1,8 +0,0 @@
-from msa_sdk.variables import Variables
-from msa_sdk.msa_api import MSA_API
-
-dev_var = Variables()
-context = Variables.task_call()
-
-ret = MSA_API.process_content('ENDED', f'Firewall service deleted', context, True)
-print(ret)
